8b46430d981fef6a12cfa56d28ef3b0bf44544b8
Kyeore Heo <91kyoheo@gmail.com>
2021-03-03 19:27:01 -0500

    first commit

---
+++ Apolis/Apolis/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Apolis
+//
+//  Created by Kyo on 3/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
